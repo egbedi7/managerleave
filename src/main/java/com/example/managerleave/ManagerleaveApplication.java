cdf076c701a317a882be818af9a4d9aaac9d3b6c
Unity imade <unity2caree@hotmail.com>
2026-08-17 16:28:25 -0600

    Initial commit

---
+++ src/main/java/com/example/managerleave/ManagerleaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.managerleave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerleaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerleaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/managerleave/ManagerleaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managerleave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerleaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
